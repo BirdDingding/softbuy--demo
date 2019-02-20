8b0bcd8f397b5061b321a3ddea75d29d0089cad6
BirdDingding <40261389+BirdDingding@users.noreply.github.com>
2019-02-20 23:17:36 +0800

    首页的优化

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ $(function () {
     gallery.slider({
         interval: 2000 //自动轮播周期，若为0则不自动播放，默认为0；
     });
-    $('.menu').on('tap','.one', function () {
+    $('.menu').on('tap', '.one', function () {
         $('.menu').toggleClass('active');
-        
+
     })
 
 
@@ -22,20 +22,29 @@ $(function () {
         }
     })
 
+    // 对头部进行注册点击跳转事件 
+    $('.productlist').on('tap', 'li', function () {
+        var href = $(this).data('href');
+        console.log(href);
+        if (href == 'history.html' || href == 'more.html') {
+            return false;
+        }
+        // 进行跳转
+        location = './pages/' + href;
+        console.log(location);
+        
+    })
+
+
     $.ajax({
-        url:'http://localhost:9090/api/getmoneyctrl',
-        success: function(data){
+        url: 'http://localhost:9090/api/getmoneyctrl',
+        success: function (data) {
             console.log(data);
-            var html = template('sale-list',data);
+            var html = template('sale-list', data);
             $('.sales-list ul').html(html);
         }
     })
 
-    $.ajax({
-
-    }
-
-    )
 
 
 })
